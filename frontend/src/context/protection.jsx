1402f55b0b75f20eb308f046bb9585b463466702
M-Durga-Prasath <mdurgap2005@gmail.com>
2025-03-28 16:20:45 +0530

    Navbar

--- frontend/src/context/protection.jsx
+++ frontend/src/context/protection.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+<<<<<<< Updated upstream:frontend/src/context/protection.jsx
 import { Auth } from "../../../backend/firebase"; // Import Firebase Auth instance
+=======
+import { Auth } from "../backend/firebase"; 
+>>>>>>> Stashed changes:src/pages/protection.jsx
 
 const ProtectedRoute = ({ children }) => {
   return Auth.currentUser ? children : <Navigate to="/login" replace />;
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import "../css/navbar.css";
+
+
+function DashboardNavbar() {
+  const navigate = useNavigate();
+
+  return (
+    <header className="dashboard-navbar">
+      <h1>Logo</h1> 
+      <nav>
+        <button onClick={() => navigate("/dashboard")}>Home</button>
+        <button onClick={() => navigate("/past-entries")}>Past Entries</button>
+        <button onClick={() => navigate("/uplifting-content")}>Uplifting Content</button>
+      </nav>
+      <div className="profile">
+        <button onClick={() => navigate("/profile")}>Profile</button>
+      </div>
+    </header>
+  );
+}
+
+export default DashboardNavbar;
