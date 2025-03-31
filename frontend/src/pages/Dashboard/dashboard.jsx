7ec2d0d7618cd627cfeeb172cd6622fc3c9e7854
M-Durga-Prasath <mdurgap2005@gmail.com>
2025-03-31 23:45:22 +0530

    straek bar done

--- frontend/src/pages/Dashboard/dashboard.jsx
+++ frontend/src/pages/Dashboard/dashboard.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import DashboardNavbar from "../Navbar/Navbar";
 import "./dashboard.css";
+import Streak from "./dashboard-features/streak";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -9,7 +10,7 @@ const Dashboard = () => {
     <>
     <DashboardNavbar/>
     <div className="dashboard-container">
-      <div className="s1">Streak Graph</div>
+      <div className="s1"><Streak/></div>
       <div className="s3">Section 3</div>
       <div className="s2">Section 2</div>
       <div className="s4">Section 4</div>
