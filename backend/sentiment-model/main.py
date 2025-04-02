533c9fa4bef270f340d199ee85c3a0b8d1d8c74d
SathishNadar <sathishmacbook01@gmail.com>
2025-04-03 00:12:53 +0530

    training sentiment model

--- backend/sentiment-model/main.py
+++ backend/sentiment-model/main.py
@@ -1,2 +1,15 @@
 from transformers import pipeline
-print(pipeline('sentiment-analysis')('I am happy'))
+
+# Load the multilingual emotion model
+emotion_analyzer = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
+
+# Example journal entries (English, Hindi, Hinglish)
+journals = [
+    "that's fire",        
+    "You are killing it man" 
+]
+
+# Get Predictions
+for journal in journals:
+    result = emotion_analyzer(journal)
+    print(f"Journal: {journal} \nEmotion: {result}\n")
