a1ea18f47473b987e0639de968ee27212fbb19ee
Mano-Bharathii <manobharathi189@gmail.com>
2025-04-05 20:33:51 +0530

    Created many functions and API inside database.js, added firebase-admin.js to handle firebase data

---
+++ backend/firebase/firebase-admin.js
@@ -0,0 +1,14 @@
+import admin from "firebase-admin";
+import { readFile } from "fs/promises";
+
+// Read the JSON file manually
+const serviceAccount = JSON.parse(
+    await readFile(new URL("./fir-testing-a3937-firebase-adminsdk-fbsvc-206487461f.json", import.meta.url))
+);
+
+// Initialize Firebase Admin
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
